484370ffcb6c5436dbe255162cbfc0302c99e8c8
imolinaa <imolinaa@ucenfotec.ac.cr>
2026-03-09 12:35:54 -0600

    inicializacion del backend

---
+++ BackEnd-CodeSprint/src/main/java/com/cenfotec/backendcodesprint/BackEndCodeSprintApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.backendcodesprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCodeSprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndCodeSprintApplication.class, args);
+    }
+
+}
---
+++ BackEnd-CodeSprint/src/test/java/com/cenfotec/backendcodesprint/BackEndCodeSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.backendcodesprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCodeSprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
